922320d57f2886d342d7c7dd0fc046ee076b5c64
Angel Singh <mary.angel904@gmail.com>
2025-06-09 17:03:19 +0530

    done

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Razorpay.Api;
 using System.IO;
 using System.Text.Json;
-using PaymentAPI.Models; // Assuming you have a Models folder with UPIPaymentRequest model
+using PaymentAPI.Models; 
 
 namespace PaymebtAPI.Controllers
 {
@@ -14,12 +14,12 @@ namespace PaymebtAPI.Controllers
         public IActionResult CreateOrder([FromBody] UPIPaymentRequest request)
         {
             Dictionary<string, object> input = new Dictionary<string, object>();
-            input.Add("amount", request.Amount * 100); // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+            input.Add("amount", request.Amount * 100);
             input.Add("currency", "INR");
             input.Add("receipt", "12121");
 
-            string key = "rzp_test_gCwU8DoXGtNICx";
-            string secret = "QisqqM9TwNdRM0hcob82rQFy";
+            string key = "";
+            string secret = "";
 
             RazorpayClient client = new RazorpayClient(key, secret);
 
